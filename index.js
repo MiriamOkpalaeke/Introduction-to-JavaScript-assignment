edf8bc44cf3096139ec07ea2c56a4b58060aebff
Miriam C. Okpalaeke <Miriam.okpalaeke@gmail.com>
2023-02-13 14:40:06 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//Write a function that multiples two numbers. 
+function multiply(a, b) {
+  return a * b;
+}
+console.log(multiply(10,10));
+
+//create variables for your first name, last name, Stutern track, skills, favourite color, year of your country’s independence.
+let firstName = 'Miriam';
+let lastName = 'Okpalaeke';
+let track = 'FrontEnd';
+let skills = 'Html, Css, Git, github, javaScript';
+let favoriteColor = "black";
+let countryIndependence = '1960';
+
+//Use these variables to form a sentence 
+console.log('my name is ' + firstName + ' ' + lastName + '. My Sturten track is '+ track + ' and I have these skills: ' + skills + '. My favourite color is ' + favoriteColor + '. Nigeria gained its independence in the year ' + countryIndependence + '.')
